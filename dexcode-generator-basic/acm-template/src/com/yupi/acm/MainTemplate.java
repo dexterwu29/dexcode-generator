132cc289f78275a43ead446f08f5321862858bc3
Dexter WU <1585829917@qq.com>
2023-12-02 21:33:31 +0800

    第三期2-进阶版：解决动态生成文件中文乱码问题

--- dexcode-generator-basic/acm-template/src/com/yupi/acm/MainTemplate.java
+++ dexcode-generator-basic/acm-template/src/com/yupi/acm/MainTemplate.java
@@ -3,32 +3,32 @@ package com.yupi.acm;
 import java.util.Scanner;
 
 /**
-* ACM ÊäÈëÄ£°å£¨¶àÊıÖ®ºÍ£©
+* ACM è¾“å…¥æ¨¡æ¿ï¼ˆå¤šæ•°ä¹‹å’Œï¼‰
 *
-* @author dex111
+* @author ä¸­æ–‡ä½œè€…
 */
 public class MainTemplate {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (scanner.hasNext()) {
-            // ¶ÁÈ¡ÊäÈëÔªËØ¸öÊı
+            // è¯»å–è¾“å…¥å…ƒç´ ä¸ªæ•°
             int n = scanner.nextInt();
 
-            // ¶ÁÈ¡Êı×é
+            // è¯»å–æ•°ç»„
             int[] arr = new int[n];
             for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
             }
 
-            // ´¦ÀíÎÊÌâÂß¼­£¬¸ù¾İĞèÒª½øĞĞÊä³ö
-            // Ê¾Àı£º¼ÆËãÊı×éÔªËØµÄºÍ
+            // å¤„ç†é—®é¢˜é€»è¾‘ï¼Œæ ¹æ®éœ€è¦è¿›è¡Œè¾“å‡º
+            // ç¤ºä¾‹ï¼šè®¡ç®—æ•°ç»„å…ƒç´ çš„å’Œ
             int sum = 0;
             for (int num : arr) {
                 sum += num;
             }
 
-            System.out.println("today's output: " + sum);
+            System.out.println("ä¸­æ–‡è¾“å‡ºï¼š" + sum);
         }
         scanner.close();
     }
--- dexcode-generator-basic/src/main/java/com/dexcode/generator/DynamicGenerator.java
+++ dexcode-generator-basic/src/main/java/com/dexcode/generator/DynamicGenerator.java
@@ -5,10 +5,10 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * 动态文件生成
@@ -47,12 +47,16 @@ public class DynamicGenerator {
 
         // 创建模板对象，加载指定模板
         String templateName = new File(inputPath).getName();
-        Template template = configuration.getTemplate(templateName);
+        // Template template = configuration.getTemplate(templateName);
 
         // 创建数据模型，从Main方法传递过来⏬
 
         // 生成
-        Writer out = new FileWriter(outputPath);
+        // Writer out = new FileWriter(outputPath);
+
+        // 第三期：解决中文乱码问题
+        Template template = configuration.getTemplate(templateName,"utf-8");
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(Paths.get(outputPath)), StandardCharsets.UTF_8));
         template.process(model, out);
 
         // 生成后关闭文件
