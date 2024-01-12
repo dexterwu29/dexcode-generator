d89f2b6fd5feb3fd429d139a61f5c1e57b0f975d
Dexter WU <1585829917@qq.com>
2024-01-12 20:40:06 +0800

    第八期5-SpringBoot 项目模板の代码生成器-测试完成

--- dexcode-generator-maker/src/main/java/com/dexcode/maker/meta/MetaManager.java
+++ dexcode-generator-maker/src/main/java/com/dexcode/maker/meta/MetaManager.java
@@ -21,7 +21,8 @@ public class MetaManager {
     }
 
     private static Meta initMeta() {
-        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+//        String metaJson = ResourceUtil.readUtf8Str("meta.json");
+        String metaJson = ResourceUtil.readUtf8Str("springboot-init-meta.json");
         Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
         // 校验配置文件，处理默认值
         MetaValidator.doValidAndFill(newMeta);
