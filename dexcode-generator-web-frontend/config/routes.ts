f139e7e9db8df2f48e87d4939197eeafd3004d84
Dexter WU <1585829917@qq.com>
2024-01-14 21:22:13 +0800

    第九期6-前端：代码生成器管理页面

--- dexcode-generator-web-frontend/config/routes.ts
+++ dexcode-generator-web-frontend/config/routes.ts
@@ -16,6 +16,12 @@ export default [
     routes: [
       { path: '/admin', redirect: '/admin/user' },
       { icon: 'table', path: '/admin/user', component: './Admin/User', name: '用户管理' },
+      {
+        icon: 'tools',
+        path: '/admin/generator',
+        component: './Admin/Generator',
+        name: '代码生成器管理',
+      },
     ],
   },
   { path: '/', redirect: '/welcome' },
